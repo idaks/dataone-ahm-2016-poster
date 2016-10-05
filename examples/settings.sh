780ef1e82dba0bc8353534a640b736f5ff3d342a
sycao5 <sycao5@gmail.com>
2016-10-05 17:49:53 -0500

    Add a new settings.sh by add yw-mat jar configuration in it.

--- examples/settings.sh
+++ examples/settings.sh
@@ -4,9 +4,11 @@ export PROJECT_ROOT=../../
 
 # define lcoation of YesWorkflow jar file
 export YW_JAR="${PROJECT_ROOT}/yw_jar/yesworkflow-0.2.1-SNAPSHOT-jar-with-dependencies.jar"
+export YW_MATLAB_JAR="${PROJECT_ROOT}/yw_jar/yw-matlab-0.2-SNAPSHOT-jar-with-dependencies.jar"
 
 # define command for running YesWorkflow
 export YW_CMD="java -jar $YW_JAR"
+export YW_MATLAB_RECON_CMD="java -jar $YW_MATLAB_JAR"
 
 # location of shared Prolog rules, scripts, and queries
 export RULES_DIR=${PROJECT_ROOT}/rules
